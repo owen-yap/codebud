bcc521aa3dbd1de606dc572b0884d430be3065c3
Hui Yong Teng <tenghuiyong@outlook.sg>
2020-12-08 07:00:19 +0800

    commit for latest change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  # devise
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
+
   has_many :questions
   has_many :messages
   has_many :skills, through: :user_skills
