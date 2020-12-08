4830c1a63fa1b4c81a24e50bf3f0ed596c3ed57f
Owen Yap <owen.y.yuhan@gmail.com>
2020-12-08 14:43:13 +0800

    added some images and added .env file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def avatar(user)
+    if user.photo.attached?
+      cl_image_tag user.photo.key, class: "avatar-sm"
+    else
+      image_tag "default_profile.jpg", class: "avatar-sm"
+    end
+  end
 end
