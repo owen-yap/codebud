b59522e3e77324118d3a5c1aca4b1a3e72227aab
Emily Chen <emchen02@gmail.com>
2020-12-08 16:18:21 +0800

    done Qn crud

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get '/uikit', to: 'pages#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :questions
 end
