afea60e39fd14b9aa311f71341cdc11cdcbf4274
Owen Yap <owen.y.yuhan@gmail.com>
2020-12-09 12:27:05 +0800

    created link to new questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,8 @@
 class QuestionsController < ApplicationController
-
   before_action :set_question, only: %i[show edit update destroy]
 
   def index
-    @questions = Question.all
+    @questions = current_user.questions
   end
 
   def new
@@ -21,7 +20,6 @@ class QuestionsController < ApplicationController
   end
 
   def show
-
   end
 
   def edit
@@ -45,7 +43,6 @@ class QuestionsController < ApplicationController
   def set_question
     @question = Question.find(params[:id])
   end
-
 
   def params_qn
     params.require(:question).permit(:title, :description, :min_price, :max_price, :start_time, :end_time, :user_id, :skills)
