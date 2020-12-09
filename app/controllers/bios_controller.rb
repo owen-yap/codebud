ffde059039b724922e9a0fcab47272e215deaf1d
Hui Yong Teng <tenghuiyong@outlook.sg>
2020-12-09 10:18:03 +0800

    enter skillset working, pending css styling

--- app/controllers/bios_controller.rb
+++ app/controllers/bios_controller.rb
@@ -6,10 +6,17 @@ class BiosController < ApplicationController
 
   def create
     @bio = Bio.new(params_bio)
-    skill = Skill.new(params[:id])
     @bio.user = current_user
-    @skill = current_user.skills
     @bio.save!
+    # extract an array of skills_ids from param
+    skill_ids = params[:bio][:user][:skill_ids]
+    # iterate thru array to get skill_id
+    skill_ids.each do |skill_id|
+      next if skill_id.empty?
+      user_skill = UserSkill.new(user: current_user)
+      user_skill.skill = Skill.find(skill_id.to_i)
+      user_skill.save!
+    end
     redirect_to root_path
   end
 
