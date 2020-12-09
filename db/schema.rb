37afb4b0fbd067f149d69f5a22ee28b392a30c52
Owen Yap <owen.y.yuhan@gmail.com>
2020-12-09 18:12:46 +0800

    Able to show all tutors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_004533) do
+ActiveRecord::Schema.define(version: 2020_12_09_095914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,7 +77,9 @@ ActiveRecord::Schema.define(version: 2020_12_09_004533) do
     t.bigint "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["question_id"], name: "index_proposals_on_question_id"
+    t.index ["user_id"], name: "index_proposals_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -148,6 +150,7 @@ ActiveRecord::Schema.define(version: 2020_12_09_004533) do
   add_foreign_key "orders", "proposals"
   add_foreign_key "payments", "orders"
   add_foreign_key "proposals", "questions"
+  add_foreign_key "proposals", "users"
   add_foreign_key "questions", "users"
   add_foreign_key "requirements", "questions"
   add_foreign_key "requirements", "skills"
