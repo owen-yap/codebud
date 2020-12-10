1b2bb4982dda01648e81ecbcf0906c7e8b11112b
Owen Yap <owen.y.yuhan@gmail.com>
2020-12-11 00:48:34 +0800

    tutors can now filter by language

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'devise'
+gem 'pg_search', '~> 2.3.0'
 
 group :development, :test do
   gem 'pry-byebug'
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -2,11 +2,13 @@ class SkillsController < ApplicationController
   def show
     @skill = Skill.find(params[:id])
   end
+
   def update
     current_user.skills.clear # destroy current skills
     array = params[:skillset][:user][:skillset] # get skill ids in an array
     array.each do |skill_id| # assign each new skill id to user and user
       next if skill_id.empty?
+
       user_skill = UserSkill.new
       skill = Skill.find(skill_id.to_i)
       user = User.find(current_user.id)
