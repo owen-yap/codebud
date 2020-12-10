f60d689bbc479b1537e5e787443fb81387246761
Owen Yap <owen.y.yuhan@gmail.com>
2020-12-10 14:56:06 +0800

    added pending selected and rejected buttons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_095914) do
+ActiveRecord::Schema.define(version: 2020_12_10_051822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_12_09_095914) do
   end
 
   create_table "bios", force: :cascade do |t|
-    t.text "content"
+    t.text "content", default: ""
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
