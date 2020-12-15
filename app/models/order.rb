b26c4c7563562a8642a310194d8619ee0814eac5
Emily Chen <emchen02@gmail.com>
2020-12-15 16:24:57 +0800

    pundit for Proposal Order Payment

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,9 @@ class Order < ApplicationRecord
   belongs_to :proposal
   has_many :reviews
   has_one :payment
-
   monetize :price_cents
+
+  def payer
+    proposal.question.user
+  end
 end
---
+++ app/policies/order_policy.rb
@@ -0,0 +1,12 @@
+class OrderPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    record.payer == user
+  end
+
+end
