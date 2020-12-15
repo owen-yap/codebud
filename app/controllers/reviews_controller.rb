63f0875d7300bef93e962f293a2bd78468d09f03
Hui Yong Teng <tenghuiyong@outlook.sg>
2020-12-15 18:01:39 +0800

    review instance can save to order

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,24 @@
 class ReviewsController < ApplicationController
   def new
+    @order = Order.find(params[:order_id])
+    @proposal = @order.proposal
+    @review = Review.new
   end
 
   def create
+    review = Review.new(review_params)
+    @order = Order.find(params[:order_id])
+    review.order = @order
+    if review.save!
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_043055) do
+ActiveRecord::Schema.define(version: 2020_12_15_090742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,7 +123,7 @@ ActiveRecord::Schema.define(version: 2020_12_15_043055) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
-    t.string "content"
+    t.text "content", default: ""
     t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
