548f81471ce081581b247267a7b04950ac64dbd3
Emily Chen <emchen02@gmail.com>
2020-12-17 10:32:18 +0800

    done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,30 @@ class SessionsController < ApplicationController
   def index
     @session = []
     @user = User.find(params[:id])
-    @questions = @user.questions
 
-    @questions.each do |question|
-      if question.selected_proposal
-        @proposal = question.selected_proposal
-        if @proposal.order.completed?
-          @session << @proposal
+    # if it is a tutor there are only proposals of questions
+    if @user.tutor
+
+      @proposals = @user.proposals
+      @proposals.each do |proposal|
+        if proposal.status == "selected" && proposal.order.completed?
+          @session << proposal
+        end
+      end
+
+    else
+      # if it is a student there are only completed question asked
+      @questions = @user.questions
+      @questions.each do |question|
+        if question.selected_proposal
+          @proposal = question.selected_proposal
+            if @proposal.order.completed?
+              @session << @proposal
+            end
         end
       end
     end
+
     return @session
   end
 end
